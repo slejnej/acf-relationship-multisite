0bdebde3bc7e1bb74c94158bfb0817ee269be494
Jernej Slejko <jslejko@flyingcolourswealth.com>
2017-09-06 11:20:28 +0100

    updated package structure

--- acf-relationship-multisite.php
+++ acf-relationship-multisite.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: Advanced Custom Fields: Relationship Multisite
 Plugin URI: https://github.com/tmconnect/acf-relationship-multisite
@@ -10,23 +11,16 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
-
-
 // 1. set text domain
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-relationship-multisite', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
+load_plugin_textdomain( 'acf-relationship-multisite', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
 // 2. Include field type for ACF5
 // $version = 5 and can be ignored until ACF6 exists
 function include_field_types_relationship_multisite( $version ) {
-	
-	include_once('acf-relationship-multisite-v5.php');
-	
+	require_once __DIR__ . '/src/acf-relationship-multisite-v5.php';
 }
 
 add_action('acf/include_field_types', 'include_field_types_relationship_multisite');	
 
-	
-?>
